b5720114d05cfbc24b8a061f8a7da9fe3a186132
Nadine-Saleh <nadinesaleh606@gmail.com>
2026-05-10 17:58:00 +0300

    test

--- src/pages/student/Exam.tsx
+++ src/pages/student/Exam.tsx
@@ -8,6 +8,7 @@ import { ExamHeader } from '../../components/layout/ExamHeader';
 import { WarningBanner } from '../../components/layout/WarningBanner';
 import { QuestionPanel } from '../../components/questions/QuestionPanel';
 import { ProctoringSidebar } from '../../components/proctoring/ProctoringSidebar';
+
 import { useExamFlow } from '../../hooks/useExamFlow';
 
 const LoadingScreen = ({ message }: { message: string }) => (
